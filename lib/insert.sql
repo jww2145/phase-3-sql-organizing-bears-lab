dfa219de1aba6241013b314aac6046dc12ada134
Joshua Wu <jww2145@columbia.edu>
2022-07-26 15:48:18 -0400

    commit tree a3c647b568a0a5d67887e492ed1221f37ea3ac34

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name,age,sex, color, temperament, alive) VALUES ('Mr. Chocolate', 14, 'M', 'Brown', 'Happy', true);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 13,'M', 'Black', 'Angry', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 21, 'F', 'White', 'Nice', false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'F', 'Brown', 'Playful', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 17, 'F','Brown', 'Old', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 15, 'M', 'Black', 'Dead', false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'White', 'Curious', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 1, 'M', 'Black', 'Baby', true);
